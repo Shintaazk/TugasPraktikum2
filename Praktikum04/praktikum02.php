1bc58980b38c0ce3d773185c94b9c9028dc34299
shintaazk <shintaanuur@gmail.com>
2024-03-13 10:52:58 +0700

    Tugas Praktikum 4

---
+++ Praktikum04/praktikum02.php
@@ -0,0 +1,118 @@
+<?php
+require_once 'header.php';
+require_once 'sidebar.php';
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Dashboard Website</h1>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <!-- Default box -->
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Judul</h3>
+
+              <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                  <i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                  <i class="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
+            <div class="card-body">
+            
+  <body>
+    <h1>Form Input Nilai Mahasiswa</h1><br>
+    <form method="POST" action="submit.php">
+
+  <div class="form-group row">
+    <label for="nama" class="col-4 col-form-label">Nama Lengkap</label> 
+    <div class="col-8">
+      <input id="nama" name="nama" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="matkul" class="col-4 col-form-label">Mata Kuliah</label> 
+    <div class="col-8">
+      <select id="matkul" name="matkul" class="custom-select">
+        <option value="web2">Pemweb 2</option>
+        <option value="basis">Basis Data</option>
+        <option value="uiux">UI UX</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="tugas" class="col-4 col-form-label">Nilai Tugas</label> 
+    <div class="col-8">
+      <input id="tugas" name="tugas" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="uts" class="col-4 col-form-label">Nilai UTS</label> 
+    <div class="col-8">
+      <input id="uts" name="uts" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="uas" class="col-4 col-form-label">Nilai UAS</label> 
+    <div class="col-8">
+      <input id="uas" name="uas" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+            </div>
+            <!-- /.card-body -->
+            <div class="card-footer">
+              Footer
+            </div>
+            <!-- /.card-footer-->
+          </div>
+          <!-- /.card -->
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+require_once 'footer.php';
+?>
---
+++ Praktikum04/praktikum03.php
@@ -0,0 +1,168 @@
+<?php
+require_once 'header.php';
+require_once 'sidebar.php';
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Dashboard Website</h1>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <!-- Default box -->
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Judul</h3>
+
+              <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                  <i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                  <i class="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
+            <div class="card-body">
+           
+    <h1>Form Registrasi IT Club</h1><br>
+   <form method="POST" action="form.php">
+  <div class="form-group row"> 
+    <label for="nim" class="col-4 col-form-label">NIM</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-adn"></i>
+          </div>
+        </div> 
+        <input id="nim" name="nim" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nama" class="col-4 col-form-label">Nama Lengkap</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-address-book"></i>
+          </div>
+        </div> 
+        <input id="nama" name="nama" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Jenis Kelamin</label> 
+    <div class="col-8">
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="gender" id="gender_0" type="radio" class="custom-control-input" value="Lakilaki"> 
+        <label for="gender_0" class="custom-control-label">Laki - laki</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="gender" id="gender_1" type="radio" class="custom-control-input" value="Perempuan"> 
+        <label for="gender_1" class="custom-control-label">Perempuan</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="studi" class="col-4 col-form-label">Progam Studi</label> 
+    <div class="col-8">
+      <select id="studi" name="studi" class="custom-select">
+        <option value="SI">Sistem Informasi</option>
+        <option value="TI">Teknik Informatika</option>
+        <option value="BD">Bisnis Digital</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Skill Web & Progaming</label> 
+    <div class="col-8">
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="skill[]" id="skill[]_0" type="checkbox" class="custom-control-input" value="html"> 
+        <label for="skill[]_0" class="custom-control-label">HTML</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="skill[]" id="skill[]_1" type="checkbox" class="custom-control-input" value="css"> 
+        <label for="skill[]_1" class="custom-control-label">CSS</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="skill[]" id="skill[]_2" type="checkbox" class="custom-control-input" value="js"> 
+        <label for="skill[]_2" class="custom-control-label">JavaScript</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="skill[]" id="skill[]_3" type="checkbox" class="custom-control-input" value="php"> 
+        <label for="skill[]_3" class="custom-control-label">PHP</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="skill[]" id="skill[]_4" type="checkbox" class="custom-control-input" value="python"> 
+        <label for="skill[]_4" class="custom-control-label">Pyhton</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="domisili" class="col-4 col-form-label">Tempat Domisili</label> 
+    <div class="col-8">
+      <select id="domisili" name="domisili" class="custom-select">
+        <option value="Bogor">Bogor</option>
+        <option value="Jakarta">Jakarta</option>
+        <option value="Bekasi">Bekasi</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="email" class="col-4 col-form-label">Email</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-envelope"></i>
+          </div>
+        </div> 
+        <input id="email" name="email" type="text" class="form-control">
+      </div>
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+  
+  
+</form>
+
+
+            </div>
+            <!-- /.card-body -->
+            <div class="card-footer">
+              Footer
+            </div>
+            <!-- /.card-footer-->
+          </div>
+          <!-- /.card -->
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+require_once 'footer.php';
+?>
